ed48b135bee63f749c2065f438e2efaf996b18ff
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-09-23 19:45:09 -0300

    Adicionado a pasta Aulas

---
+++ Aulas/Aula4/Exemplo1/Script.js
@@ -0,0 +1,16 @@
+/*Exemplo 1
+
+Joao Pedro Fiuza */
+
+console.log("Olá mundo ")
+
+console.log("Minha primeira página criada")
+
+prompt("Qual seu nome")
+
+const nome = "Joao Pedro Fiuza"
+const idade = 24
+
+console.log(" O nome é:", nome, "E sua idade é:", idade)
+
+
---
+++ Aulas/Aula4/Exercicios1/exercicio.js
@@ -0,0 +1,6 @@
+const nome = "Joao Pedro"
+const sobrenome = "Fiuza Wilges"
+const idade = "24"
+const souEstudante = true
+
+console.log("Meu nome é:", nome, "Meu sobrenome é", sobrenome, "Minha idade é:",idade, "E eu sou um bom estudante",souEstudante )
