97978f460c0fa6639a4fa8652b136a5e094a6f52
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-09-24 21:54:01 -0300

    Adcionado os exercicios da aula 6

---
+++ Aulas/Aula6/Exercicios/Atividades.js
@@ -0,0 +1,51 @@
+/*let a = 10
+// let é variável onde "a" equivale a 10 neste momento
+let b = 10
+// let é uma variável onde "b" equivale a 10 neste momento
+console.log(b)  
+
+b = 5
+// aqui o b como era let foi atribuido a 5 no final
+console.log(a, b)    
+/* O console imprimi o 10 que apresenta no let b (onde let é uma variável) isso no primeiro b
+e no segundo ele imprimi  10 e o número 5 pois o let é uma variável mutavel e ela mudou no segundo console log
+
+let a = 10
+// a variável a é 10
+let b = 20
+// a variável b é 20
+c = a
+// o valor de c é igual o valor do let a no caso é 10
+b = c
+// o valor de b é igual o valor de c onde o c é atribuído no valor a no caso 10
+a = b
+// aqui aprsenta que o valor de a é a variável b onde b é o valor da variável c por tanto c é 10
+
+console.log(a, b, c)    
+//imprimi tudo 10 no caso 3 x o valor 10
+
+
+let p = prompt("Quantas horas você trabalha por dia?")
+// O prompt imprime a frase acima para o usuario escrever 
+let t = prompt("Quanto você recebe por dia")
+// O prompt imprime a frase acima para o usuario escrever 
+console.log("Voce recebe",t,"/",p,"por hora")
+// Imprimi no console da pagina a digitação do usario e depois aparece 
+
+// p horasTrabalhadas
+// t ganhosPorDia
+*/
+let nome 
+
+let idade
+
+console.log(typeof(nome),typeof(idade)) //Unexpected identifier pois não 
+
+
+
+
+
+
+
+
+
