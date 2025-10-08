a60bccbc7d8e51a2663f8299af179be52477be7d
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-10-07 21:47:16 -0300

    aula9

--- Aulas/Aula8/ExerciciosDeFixaçao.js
+++ Aulas/Aula8/ExerciciosDeFixaçao.js
@@ -5,8 +5,56 @@
 
 // Sugiro que ele troque o const soma per console.log
 
-let n1 = prompt("aa")
-let n2 = prompt("bb")
+/*2
 
-console.log(n1 + n2)
+let primeiroNumero = prompt ("Digite um número!")
+let segundoNumero = prompt ("Digite outro número!")
 
+ Por que puxa em string e pelo correto deveria sair em número  "Number"*/
+
+ /* 3
+
+ let suaIdade= prompt ("Qual sua idade ?")
+ let idadeAmigo= prompt("Qual a idade do seu melhor amigo ?")
+
+ let suaIdade2 = Number(suaIdade)
+ let  idadeAmigo2 = Number (idadeAmigo)
+
+
+console.log("Sua idade é maior do que a do seu melhor amigo ?",idadeAmigo2 >= suaIdade2)
+console.log(" A diferença de idade é:",suaIdade - idadeAmigo)*/
+
+/*
+
+let numeroPar = prompt("Insira um número par:")
+console.log(numeroPar % 2)
+
+// Sempre aparece o valor 1 no impar pois é o resto da divisão */
+
+/*3
+
+let usarioIdade = prompt("Qual sua idade ?")
+
+let usarioIdade2 = Number(usarioIdade)
+
+console.log("Sua idade em meses é", usarioIdade2 * 12)
+console.log("Sua idade em dias é", usarioIdade2 * 365)
+console.log("Sua idade em horas é", usarioIdade2 * 24)*/
+
+/*4
+
+let usarioNumero = prompt("fala ai o n1")
+let usarioNumero2 = prompt("fala ai o n2")
+let usarioNumeroN = Number(usarioNumero)
+let usarioNumero2N = Number(usarioNumero2)
+let restiDivisao5 = usarioNumeroN % usarioNumero2N
+let restiDivisao6 = usarioNumero2N % usarioNumeroN
+
+console.log("O primeiro número é maior que o segundo ?", usarioNumeroN > usarioNumero2N)
+console.log("O primeiro número é igual ao segundo ?", usarioNumeroN == usarioNumero2N)
+console.log("O primeiro número é divisível pelo segundo ?", restiDivisao5 >= 0)
+console.log("O segundo número é divisível pelo primeiro ?", restiDivisao6 >= 0)*/
+
+
+let kelvin  = ((GRAUS_FAHRENHEIT - 32)*(5/9) + 273.15)
+let = (GRAUS_FAHRENHEIT) = (GRAUS_CELSIUS)*(9/5) + 32
---
+++ Aulas/Aula9/Ex.js
@@ -0,0 +1,29 @@
+/*const raçaGato = ["Frajola","Laranja","Preto","Branco","Siames"]
+const usuario = Number(prompt("Insira um número de 0 a 4 e eu urei lhe dizer a posição da raça escolhida",))
+console.log(raçaGato[usuario])
+
+const numeros = [1,2,3]
+numeros.push(4)
+console.log(numeros)
+
+numeros.push(5,6,7)
+console.log(numeros)
+
+const meusPeixes = ["palhaço", "mandarim", "esturjao"]
+meusPeixes.pop()
+console.log(meusPeixes)
+
+const letras =["A","B","C","D","E","F","G","H"]
+
+letras.splice(2,1)
+console.log(letras)*/
+
+
+
+
+const numeros = ["1","2","3","4","5","6"]
+console.log(numeros.length)
+numeros.push("7")
+numeros.splice(3,2)
+console.log(numeros.length)
+console.log(numeros)
