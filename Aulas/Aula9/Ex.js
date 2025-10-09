abeff2d06a85383a929eaa54bc24608e96c75da7
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-10-08 21:48:45 -0300

    Aula exercicios dificeis estude +

--- Aulas/Aula9/Ex.js
+++ Aulas/Aula9/Ex.js
@@ -16,9 +16,7 @@ console.log(meusPeixes)
 const letras =["A","B","C","D","E","F","G","H"]
 
 letras.splice(2,1)
-console.log(letras)*/
-
-
+console.log(letras)
 
 
 const numeros = ["1","2","3","4","5","6"]
@@ -27,3 +25,66 @@ numeros.push("7")
 numeros.splice(3,2)
 console.log(numeros.length)
 console.log(numeros)
+
+let array
+console.log('a.', array) //undefined
+
+array = null
+console.log('b', array)
+
+array = [3,4,5,6,7,8,9,10,11,12,13] //null
+console.log('c', array.length)
+
+let i = 0 
+console.log('d', array[i]) // 11 
+
+array[I+1] = 19
+console.log('e', array) //3
+
+const valor = array[i+6] //19
+console.log('f', valor)
+
+const frase = prompt("Digite uma frase")
+console.log(frase.toUpperCase().replaceAll("A","I"), frase.length)
+//Vau aparecer em maiuscula e contar as caractérias
+
+let email = prompt("Qual seu e-mail ?")
+let nome = prompt("Qual seu nome ?")
+console.log("O email do usúario", email, "foi cadastrado com sucesso. Seja bem-vinda(o),", nome )
+
+const comidas = ["sushi", "churrasco", "pizza", "lamen", "hamburguer"]
+console.log(comidas)
+
+console.log(`Essas são as minhas comidas preferidas: ${comidas}`)
+console.log(comidas[0])
+console.log(comidas[1])
+console.log(comidas[2])
+console.log(comidas[3])
+console.log(comidas[4])
+
+const novaComida = prompt("Qual é a sua comida preferida?");
+comidas[1] = novaComida;
+
+console.log("Nova lista de comidas preferidas:", comidas);
+
+let listaDeTarefas = []
+const tarefa1 = prompt("Digite a primeira tarefa")
+const tarefa2 = prompt("Digite a segunda tarefa")
+const taref3 = prompt("Digite a terceira tarefa")
+
+let novaLista = listaDeTarefas.push(tarefa1,tarefa2,taref3)
+console.log(listaDeTarefas)*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
