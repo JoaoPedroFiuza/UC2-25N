c8b635728d438c9b68633d06aaa8046f52de292b
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-10-10 21:49:41 -0300

    Look mmaluco

---
+++ Aulas/Aula10/LookMaluco.js
@@ -0,0 +1,40 @@
+/*const valor1 = Number(prompt("Número de acessórios"))
+const valor2 = Number(prompt("Número de cores"))
+const valor3 = Number(prompt("Fator brilho"))
+const pontuaçao = ((valor1*20)+(valor2*3)+(valor3*10))
+const metaPontuacao = 80
+const resultado = pontuaçao >= metaPontuacao
+if (resultado === true) {console.log('Aprovado')}
+else console.log((metaPontuacao - pontuaçao), pontuaçao)*/
+
+let descriçao = prompt('Descrição do Look')
+
+let corPrincipal = prompt('Cor de destaque do Look')
+
+console.log = (descriçao, corPrincipal)
+
+console.log = (descriçao.length)
+
+let temTamanhoAdequado = (descriçao.length) >= 20
+
+let palavraChave = descriçao.toLocaleLowerCase ()
+
+let brilho = palavraChave.indexOf !== -1
+
+let listras = palavraChave.indexOf !== -1
+
+let lookAprovado = temTamanhoAdequado&&(tem)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
