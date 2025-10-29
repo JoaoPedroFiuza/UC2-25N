75543d215d0ac5a40250fde0b359242e43282095
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-10-28 21:44:47 -0300

    Exercicios

--- Aulas/Aula13/ExercicioAula13.js
+++ Aulas/Aula13/ExercicioAula13.js
@@ -22,14 +22,14 @@ let mensagem = ""
 for(let palavra of palavras){
     mensagem+= palavra+" "
 }
-console.log(mensagem)*/
+console.log(mensagem)
 
 // EXERCICIO 1
 let valor = 0
 for (let i = 0; i < 5; i++) {
     valor += i
 }
-console.log(valor)
+console.log(valor)/*No console log apresenta os valores somando 1 até chegar no 5 dai ela para de somar em 1 em 1
 
 
 // EXERCICIO 2
@@ -37,11 +37,12 @@ const lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
 
 for (let numero of lista) {
     if (numero > 18) {
-        console.log(numero)
-    }
-}
+        console.log(numero)/* Vai aparecer todos os números em lista maiores que 18.
+             Sim é suficiente, porém teria que tirar o if  e deixar só o console*/
+  //  }
+//}
 
-// EXERCICIO 3
+/* EXERCICIO 3
 
 const quantidadeTotal = Number(prompt('Digite a quantidade de linhas:'))
 let quantidadeAtual = 0
@@ -52,8 +53,36 @@ while(quantidadeAtual < quantidadeTotal){
     }
     console.log(linha)
     quantidadeAtual++
+    //REPRESENTA 1 ASTERISCO POR LINHA ATÉ CHEGAR NA LINHA INDICADA POR EX SE O USUARIO COLOCAR 3 LINHAS VAI APARECE 3 ASTERISOS NA 3 LINHA E ANTERIORMENTE 2 E ASSIM POR DIANTE
     
 }
 
+let nomeAnimais = []
+let bichinhos = Number(prompt('Você tem quantos animais de estimação ?'))
+
+if(bichinhos === 0){
+    console.log('Você deveria adora um !')
+} else {
+    for(let i = 0; i < bichinhos; i++){
+      let nomeAnimal = prompt('Digite o nome do animal')
+      nomeAnimais.push(nomeAnimal)
+    }
+}
+console.log(nomeAnimais)*/
+
+//2
+
+arrayOriginal = [1,2,3,4,5]
+for(let i = 0; i < 5; i++){
+    const array = arrayOriginal[i]
+    console.log(array)
+
+}
+
+arrayOriginal1 = [1,2,3,4,5]
+for(let i = 0; i < 5; i++){
+    const array1 = arrayOriginal1[i]
+    console.log(array1/10)
+}
 
 
