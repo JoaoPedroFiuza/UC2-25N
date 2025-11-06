26f1c81fdd23c53efcc2bf93e11a34ad114daa42
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-11-05 21:47:58 -0300

    aula 13

---
+++ Aulas/Aula14/exer.funçao.js
@@ -0,0 +1,49 @@
+/*function doisNumeros(numero1, numero2){
+    const doisNumeros1 = numero1 * numero2
+return doisNumeros1
+}
+
+const doisNumeros3 = doisNumeros(1,2)
+console.log(doisNumeros(1,2))
+
+
+
+    
+let ArrayNumeros = [7,77,777,7777]
+function divisao(array){
+    let novoArray = []
+    novoArray.push(array[0]/2)
+    novoArray.push(array[array.length-1]/2)
+    return novoArray
+}
+console.log(divisao(ArrayNumeros))
+
+let doisNumeros= function(numero1, numero2){
+   let doisNumeros1 = numero1 + numero2
+    console.log(doisNumeros1)
+}
+
+doisNumeros(2,3)
+
+let soma = function(){
+    return 2 + 2
+}
+console.log('A soma é:'soma(2,2))*/
+
+const determinaMonstro = (temPeleGelada, transformaComLuaCheia, brilhaAoSol, temSedeDeSangue) => {
+switch (determinaMonstro){
+case 'Vampiro':
+    console.log()
+}
+}
+
+
+
+
+
+
+
+
+
+
+
