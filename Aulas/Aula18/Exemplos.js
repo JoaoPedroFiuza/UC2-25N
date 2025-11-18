715cefc13f5ece3d81a50b5fed16c63f8da386c4
JoaoPedroFiuza <jpfwilges@gmail.com>
2025-11-17 21:51:11 -0300

    Aula18

---
+++ Aulas/Aula18/Exemplos.js
@@ -0,0 +1,56 @@
+/*let matriz = [
+    [1,2,3],
+    [4,5,6]
+];
+
+
+let matriz = [];
+
+matriz[0]=[1,2,3]
+matriz[1]=[4,5,6]
+matriz[2]=[7,8,9]
+
+let matriz = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+];
+
+matriz[1][2] = 1;
+
+console.log(matriz);
+
+
+let matriz = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let soma = 0
+for(let i = 0 ; i < matriz.length ; i++){
+  for(let j=0;j < matriz[i].length ; j++){
+    soma += matriz[i][j]
+console.log(matriz[i][j],)}}
+console.log(soma)*/
+
+
+let matriz = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+    [13, 14, 15, 16]
+];
+
+let soma = 0
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        soma += matriz[i][j]
+        console.log(matriz[i][j],)
+    }
+}
+console.log('A soma de tudo da',soma)
+
+
+
+
